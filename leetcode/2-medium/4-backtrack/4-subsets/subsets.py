951691461b5779f730d5f65b7b387dc35dbbf61e
Raymond Chi <raychi@gmail.com>
2018-05-24 15:57:52 -0700

    leetcode finished medium backtracking section.

--- leetcode/2-medium/4-backtrack/4-subsets/subsets.py
+++ leetcode/2-medium/4-backtrack/4-subsets/subsets.py
@@ -1,9 +1,12 @@
 # https://leetcode.com/explore/interview/card/top-interview-questions-medium/109/backtracking/796/
+# https://leetcode.com/problems/subsets/description/
 
 # Subsets
 #
 # Given a set of distinct integers, nums, return all possible subsets
 # (the power set). The solution set must not contain duplicate subsets.
+
+import unittest
 
 
 class Solution:
@@ -13,4 +16,72 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        # return self.build_powerset(nums)
+        return self.build_powerset2(nums)
 
+    # Note: start with the empty set [], for each item in input array,
+    #   add to all items in previous powerset. Repeat for next item.
+    #   Total 2^n items in powerset for n items (since each time it
+    #   doubles).
+    
+    # 10 / 10 test cases passed.
+    # Status: Accepted
+    # Runtime: 44 ms (beats 93.67% py3)
+    def build_powerset(self, A):
+
+        res = [[]]
+        for x in A:  # for each item x in nums array
+            s2 = []
+            for v in res:  # for each item in previous powerset
+                v2 = v + [x]  # create new item by add x to it
+                s2.append(v2)  # add new item to temp list
+            res.extend(s2)  # add items s2 to result
+        return res
+
+        # shorter version from discussion, damn it
+        # res = [[]]
+        # for num in sorted(nums):
+        #     res += [item + [num] for item in res]
+        # return res
+
+    # Note: powerset can also be built using bit masks. For n items,
+    #   the powerset can be simply viewed as all unique sequences of
+    #   selections (of size n). ie, ([no, no], [no, yes], [yes, no],
+    #   [yes, yes]) where yes/no indicates whether nth item is in the
+    #   list or not.
+    #
+    #   This means we can also build the powerset by iterate through
+    #   all the numbers between 0 to 2^n (for n items), and using the
+    #   number's binary representation to select whether an item is in
+    #   it or not.
+    def build_powerset2(self, A):
+
+        def map_to_set(k):  # map integer k to a set of elements
+            val = []  # return set
+            idx = 0  # index into element list A
+            while k:  # while k has bits set
+                if k & 1:  # if LSB is set
+                    val.append(A[idx])  # add ith element to set
+                idx += 1
+                k >>= 1  # shift right by 1
+            return val
+
+        n = 1 << len(A)  # calc 2^n
+        res = []
+        for i in range(n):
+            res.append(map_to_set(i))
+        return res
+
+
+class TestSubsets(unittest.TestCase):
+
+    def setUp(self):
+        self.sol = Solution()
+
+    def test1(self):
+        print(self.sol.subsets([1,2,3]))
+        print(self.sol.subsets([1,2,3, 4]))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ leetcode/2-medium/4-backtrack/5-wordsearch/word_search.py
@@ -0,0 +1,88 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-medium/109/backtracking/797/
+# https://leetcode.com/problems/word-search/description/
+
+# Word Search
+#
+# Given a 2D board and a word, find if the word exists in the grid.
+# The word can be constructed from letters of sequentially adjacent
+# cell, where "adjacent" cells are those horizontally or vertically
+# neighboring. The same letter cell may not be used more than once.
+
+import unittest
+
+
+class Solution:
+    
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        return self.my_search(board, word)
+
+    # Note: run dfs on all matching 1st chars in board. in dfs, check
+    #   whether char is a match or not, if so, recurse on all 4
+    #   neighbors (clear char at current board position before
+    #   recursive dfs() call, and restore char after all 4 calls).
+    
+    # 87 / 87 test cases passed.
+    # Status: Accepted (on 1st submit, yeah!)
+    # Runtime: 260 ms
+    def my_search(self, M, word):
+
+        def dfs(pos, s):  # search from pos for string
+
+            if not s:  # exhausted input, done
+                return True
+
+            r, c = pos[0], pos[1]  # check within board boundry
+            if r < 0 or r >= rows:
+                return False
+            if c < 0 or c >= cols:
+                return False
+
+            if M[r][c] != s[0]:  # if current pos not match, done
+                return False
+
+            save = M[r][c]  # clear current char (so child dfs don't see it)
+            M[r][c] = None
+            retval = (dfs((r, c+1), s[1:]) or  # right
+                        dfs((r, c-1), s[1:]) or  # left
+                        dfs((r-1, c), s[1:]) or  # up
+                        dfs((r+1, c), s[1:]))    # down
+            M[r][c] = save
+
+            return retval
+
+        if not word:
+            return False
+
+        rows = len(M)
+        cols = len(M[0])
+
+        for i in range(rows):
+            for j in [x for x, v in enumerate(M[i]) if v == word[0]]:
+                if dfs((i, j), word):
+                    return True
+
+        return False
+
+
+class TestWordSearch(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+        self.board = [
+            ['A', 'B', 'C', 'E'],
+            ['S', 'F', 'C', 'S'],
+            ['A', 'D', 'E', 'E']
+        ]
+
+    def test1(self):
+        self.assertTrue(self.sol.exist(self.board, 'ABCCED'))
+        self.assertTrue(self.sol.exist(self.board, 'SEE'))
+        self.assertFalse(self.sol.exist(self.board, 'ABCB'))
+
+
+if __name__ == '__main__':
+    unittest.main()
